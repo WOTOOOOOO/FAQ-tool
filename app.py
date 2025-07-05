cdc7cf717bb0ab18e6747857d029c18c9900d6e5
woto <giorgichavchavadze132@gmail.com>
2025-07-05 10:50:21 +0400

    Add graph integration to application

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from regulations_service import RegulationService
 from calendar_service import CalendarService
 from main import UniversityQueryAgent
 from student_generator_util import generate_student_data_csv
+from nodes import UniversityGraph
+from langchain_core.messages import AIMessage
 
 sys.modules['torch.classes'] = None
 
@@ -19,19 +21,28 @@ class QueryApp:
         self.service_init_key = "services_initialized"
         self.student_data_key = "student_data_generated"
         self.faq_agent_key = "faq_agent_initialized"
+        self.university_graph_key = "university_graph_initialized"
 
     def setup(self):
         self._generate_students()
         self._initialize_services()
         self._initialize_chat_history()
         self._initialize_faq_agent(FAISS_REGULATIONS_INDEX_NAME, FAISS_CALENDAR_INDEX_NAME, STUDENTS_CSV)
+        self._initialize_university_graph()
 
     def run(self):
+
         self.setup()
         st.title(self.title)
         query = self._get_user_input()
         self._handle_query_submission(query)
         self._display_chat_history()
+
+    def _initialize_university_graph(self):
+        if self.university_graph_key not in st.session_state:
+            graph = UniversityGraph(st.session_state[self.faq_agent_key])
+            graph.build_langgraph()
+            st.session_state[self.university_graph_key] = graph
 
     def _initialize_faq_agent(self, faiss_regulations_index: str, faiss_calendar_index: str, students_csv: str):
         if self.faq_agent_key not in st.session_state:
@@ -60,13 +71,51 @@ class QueryApp:
         return st.text_area("Enter your query:", key=self.query_key)
 
     def _handle_query_submission(self, query):
+        # Submit button logic
         if st.button("Submit") and query:
             response = self._generate_response(query)
-            st.session_state[self.chat_history_key] = [{"query": query, "response": response}] + st.session_state[
-                self.chat_history_key]
+            st.session_state["last_query"] = query
+            st.session_state["last_response"] = response
+
+            if not response.get("hitl_ui_needed"):
+                st.session_state[self.chat_history_key] = [
+                                                              {
+                                                                  "query": query,
+                                                                  "response": {"output": response.get("output")}
+                                                              }
+                                                          ] + st.session_state[self.chat_history_key]
+
+        # Approve UI and logic
+        if st.session_state.get("last_response") and st.session_state["last_response"].get("hitl_ui_needed"):
+            query = st.session_state["last_query"]
+            response = st.session_state["last_response"]
+            context = response.get("context")
+            llm_response = response.get("output")
+
+            st.markdown(f"Query: {query}")
+            st.text_area("Retrieved Context:", value=context, height=200, disabled=True)
+            edited_response = st.text_area("Review/Edit LLM Answer:", value=llm_response, height=150,
+                                           key="edit_llm_response")
+
+            if st.button("Approve"):
+                st.session_state[self.chat_history_key] = [
+                                                              {
+                                                                  "query": query,
+                                                                  "response": {"output": edited_response}
+                                                              }
+                                                          ] + st.session_state[self.chat_history_key]
+                st.session_state[self.university_graph_key].university_agent.memory.chat_memory.messages[
+                    -1] = AIMessage(
+                    edited_response or "No output."
+                )
+
+                # Cleanup after approval
+                del st.session_state["last_query"]
+                del st.session_state["last_response"]
+                st.rerun()
 
     def _generate_response(self, query):
-        return st.session_state[self.faq_agent_key].generate_response(query)
+        return st.session_state[self.university_graph_key].generate_response(query)
 
     def _display_chat_history(self):
         st.write("### Chat History:")
---
+++ nodes.py
@@ -0,0 +1,163 @@
+from pydantic import BaseModel
+from typing import Dict, Any, Optional
+from langgraph.graph import StateGraph, END
+from langchain_core.documents import Document
+from langchain_core.runnables import RunnableLambda
+from langchain_core.agents import AgentFinish
+
+
+class GraphState(BaseModel):
+    query: str
+    retrieved: Optional[list[Document]] = None
+    intermediate_response: Optional[str] = None
+    metadata: Optional[Dict[str, Any]] = None
+    output: Optional[str] = None
+    context: Optional[str] = None
+    hitl_ui_needed: Optional[str] = None
+    selected_tool: Optional[str] = None
+
+
+class UniversityGraph:
+    def __init__(self, university_agent):
+        self.university_agent = university_agent
+        self.graph = None
+
+    def decide_route(self, query):
+        plan = self.university_agent.agent.agent.plan(
+            input=query,
+            chat_history=self.university_agent.memory.chat_memory.messages,
+            intermediate_steps=[]
+        )
+
+        if isinstance(plan, AgentFinish):
+            return "None", "default_node"
+
+        tool_name = plan.tool
+
+        if tool_name == "Calendar events retriever":
+            return tool_name, "calendar_node"
+        if tool_name == "Current Date and Time tool":
+            return tool_name, "date_time_node"
+        if tool_name == "Pandas student Data Frame Tool":
+            return tool_name, "pandas_node"
+        if tool_name == "University regulations retriever":
+            return tool_name, "retrieve"
+
+        return tool_name, "default_node"
+
+    def default_node(self, state: GraphState) -> GraphState:
+        guided_query = (
+            f"Without using any tools respond to this query {state.query}"
+        )
+
+        output = self.university_agent.agent.invoke({"input": guided_query})
+        return state.model_copy(update={
+            "output": output.get("output", output),
+        })
+
+    def decision_routing_node(self, state: GraphState) -> GraphState:
+        tool, route = self.decide_route(state.query)
+        return state.model_copy(update={
+            "metadata": {"next_node": route},
+            "selected_tool": tool
+        })
+
+    def regulations_rag_retrieval_node(self, state: GraphState) -> GraphState:
+        query = state.query
+        docs_with_scores = self.university_agent.vector_store_regulations.similarity_search_with_score(query, k=3)
+        retrieved_docs = [doc for doc, score in docs_with_scores]
+        scores = [score for _, score in docs_with_scores]
+        max_possible_distance = 2.0
+        normalized_scores = [max(0.0, 1 - (s / max_possible_distance)) for s in scores]
+        confidence = round(sum(normalized_scores) / len(normalized_scores), 3)
+
+        return state.model_copy(update={
+            "retrieved": retrieved_docs,
+            "intermediate_response": "Documents retrieved based on FAISS search.",
+            "metadata": {"confidence": confidence}
+        })
+
+    def rag_answer_generation_node(self, state: GraphState) -> GraphState:
+        context = "\n\n".join([doc.page_content for doc in state.retrieved])
+        tool = state.selected_tool or "a relevant tool"
+
+        guided_query = (
+            f"Use the tool '{tool}' to answer this question. "
+            f"Now, answer the user query:\n{state.query}"
+        )
+
+        try:
+            output = self.university_agent.agent.invoke({"input": guided_query})
+            return state.model_copy(update={
+                "output": output.get("output", output),
+                "context": context
+            })
+        except Exception as e:
+            return state.model_copy(update={
+                "output": f"Agent failed while trying to use {tool}: {str(e)}",
+                "context": context
+            })
+
+    def calendar_rag_retrieval_node(self, state: GraphState) -> GraphState:
+        query = state.query
+        docs_with_scores = self.university_agent.vector_store_calendar.similarity_search_with_score(query, k=20)
+        retrieved_docs = [doc for doc, score in docs_with_scores]
+        scores = [score for _, score in docs_with_scores]
+        max_possible_distance = 2.0
+        normalized_scores = [max(0.0, 1 - (s / max_possible_distance)) for s in scores]
+        confidence = round(sum(normalized_scores) / len(normalized_scores), 3)
+
+        return state.model_copy(update={
+            "retrieved": retrieved_docs,
+            "intermediate_response": "Documents retrieved based on FAISS search.",
+            "metadata": {"confidence": confidence}
+        })
+
+    def rag_final(self, state: GraphState) -> GraphState:
+        if state.metadata.get("confidence", 1.0) < 0.8:
+            return state.model_copy(update={"hitl_ui_needed": True})
+        return state.model_copy()
+
+    def generate_response(self, query: str) -> Dict[str, Any]:
+        result_state = self.graph.invoke(GraphState(query=query))
+        return result_state
+
+    def build_langgraph(self):
+        builder = StateGraph(GraphState)
+
+        builder.add_node("route", RunnableLambda(self.decision_routing_node))
+        builder.add_node("default_node", RunnableLambda(self.default_node))
+        builder.add_node("regulations_retrieve", RunnableLambda(self.regulations_rag_retrieval_node))
+        builder.add_node("calendar_retrieve", RunnableLambda(self.calendar_rag_retrieval_node))
+        builder.add_node("generate", RunnableLambda(self.rag_answer_generation_node))
+        builder.add_node("rag_final", RunnableLambda(self.rag_final))
+
+        builder.set_entry_point("route")
+
+        builder.add_conditional_edges(
+            "route",
+            lambda state: state.metadata["next_node"],
+            {
+                "retrieve": "regulations_retrieve",
+                "pandas_node": END,
+                "calendar_node": "calendar_retrieve",
+                "date_time_node": END,
+                "default_node": "default_node",
+                None: END
+            }
+        )
+
+        # Default path
+        builder.add_edge("default_node", END)
+
+        # University RAG path
+        builder.add_edge("regulations_retrieve", "generate")
+        builder.add_edge("generate", "rag_final")
+        builder.add_edge("rag_final", END)
+
+        # Calendar RAG path
+        builder.add_edge("calendar_retrieve", "generate")
+        builder.add_edge("generate", "rag_final")
+        builder.add_edge("rag_final", END)
+
+        self.graph = builder.compile()
